cfd459845bae290e746a9e2d3f0dfa7bafb77d33
PierreTDX <pierre.tondeux@gmail.com>
2025-03-04 22:24:25 +0100

    res.header

--- server/server.js
+++ server/server.js
@@ -26,6 +26,19 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // CORS pre-flight request (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
+  next();
+});
+
 // Request payload middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
