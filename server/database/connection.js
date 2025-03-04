9334b93f618671ff30dbfc3041562e95a686b48f
PierreTDX <pierre.tondeux@gmail.com>
2025-03-04 23:02:16 +0100

    strictQuery

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const databaseUrl = process.env.DATABASE_URL
 
+// Ajouter cette ligne pour définir explicitement l'option strictQuery
+mongoose.set('strictQuery', false); // ou true si tu préfères
+
 module.exports = async () => {
   try {
     await mongoose.connect(databaseUrl, { useNewUrlParser: true })
